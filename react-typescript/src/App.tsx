c213ea5c06a8650ea86812518784d0a1b1c48efc
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2026-05-26 08:35:31 +0530

    Habit form

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,3 +1,4 @@
+import HabitForm from './Components/HabitForm';
 import Header from './Components/Header';
 import './index.css'
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className='max-w-3xl mx-auto p-4 flex flex-col gap-4'>
       <Header/>
+      <HabitForm/>
     </div>
   )
 }
