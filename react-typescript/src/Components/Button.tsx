9f0d4741b1269656c4a586f41730c96833a46659
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2026-06-13 19:49:51 +0530

    Update Button.tsx

--- react-typescript/src/Components/Button.tsx
+++ react-typescript/src/Components/Button.tsx
@@ -1,7 +1,9 @@
 import type { ComponentProps } from "react";
 
+type Variant = 'primary' | 'secondary' | 'ghost-destructive';
+
 type ButtonProps = {
-   variant?: 'primary' | 'secondary' | 'ghost-destructive';
+   variant?: Variant;
     //name: string
     //children: React.ReactNode; 
     //disabled?:boolean
@@ -15,5 +17,13 @@ function Button({ ...props }: ButtonProps) {
     <button {...props} className='bg-purple-600 text-white px-4 py-2 rounded-md hover:bg-purple-900 transition-colors disabled:opacity-50 disabled:cursor-not-allowed'>{props.children}</button>
   )
 }
+
+function getVariantStyles(variant: Variant) {
+    switch(variant){
+      case 'primary':
+           
+    }
+}
+
 
 export default Button
